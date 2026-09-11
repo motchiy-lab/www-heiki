3609c0a23b480ea667e0ed555b6a8740e4f536a7
Judo Master <hc4121109@gs.seito-fku.ed.jp>
2026-09-11 12:10:42 +0900

    Update astro.config.mjs
    Signed-off-by: Judo Master <hc4121109@gs.seito-fku.ed.jp>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://heiki.motchiy.com',
   output: 'server',
   adapter: cloudflare({
     imageService: 'passthrough'
